322e81d624da88c983d881959038c48386b743e1
Jahnavi5989 <bjahnavi5989@gmail.com>
2022-06-16 16:19:06 +0530

    initial commit

---
+++ employee_register/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path,include
+from .import views
+
+urlpatterns = [
+   path('',views.employee_form,name="employee_insert"), #get and post req. for insert operation
+   path('<int:id>/',views.employee_form,name="employee_update"), #get and post req. fro update operation
+   path('delete/<int:id>/',views.employee_delete,name='employee_delete'),
+   path('list/',views.employee_list,name='employee_list') # get req. to retrieve and display all records
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
